94acf8e7742d20b842ed4c205f262b53b6ef6878
nageetaw <nigeetawadhwani@gmail.com>
2025-11-07 14:07:27 +0100

    completed server using open AI model

--- server/main.py
+++ server/main.py
@@ -4,8 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, Settings
 from llama_index.vector_stores.pinecone import PineconeVectorStore
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.llms.openai import OpenAI
 from pinecone import Pinecone
+from llama_index.llms.openai import OpenAI
 import os
 import shutil
 from dotenv import load_dotenv
@@ -29,9 +29,16 @@ PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 pc = Pinecone(api_key=PINECONE_API_KEY)
 pinecone_index = pc.Index(os.getenv("PINECONE_INDEX"))
 
+# Embedding Model (for Pinecone)
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+# Model for inference
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm = OpenAI(api_key=OPENAI_API_KEY, model="gpt-3.5-turbo")
+Settings.llm=llm
+
+
 vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
 UPLOAD_DIR = Path("uploads")
 UPLOAD_DIR.mkdir(exist_ok=True)
@@ -56,8 +63,8 @@ async def upload_pdf(file: UploadFile = File(...)):
 
 @app.post("/query")
 async def query_doc(question: str = Form(...)):
-    # Query using the index
     index = VectorStoreIndex.from_vector_store(vector_store)
+    # Later we will update this to chat engine
     query_engine = index.as_query_engine()
     response = query_engine.query(question)
     return {"response": str(response)}
